84b2a8b8e701328937a1d469a9c80b43137ba083
13935 <hl13935@ly.com>
2016-06-12 14:54:59 +0800

    Using Data Annotations for Model Validation

---
+++ MvcMusicStore/Controllers/StoreManagerController.cs
@@ -0,0 +1,130 @@
+﻿/*
+ * 无法检索“MvcMusicStore.Models.Album”的元数据。Using the same DbCompiledModel to create contexts against different types of database servers is not supported. Instead, create a separate DbCompiledModel for each type of server being used.
+ * 解决办法
+ * 第一就是把webconfig中的数据库配置   providerName="System.Data.SqlServerCe.4.0 修改成 providerName="System.Data.SqlClient"  这样就可以添加成功控制器了
+ * 第二 生成控制器以后 在把  providerName="System.Data.SqlClient  改回 providerName="System.Data.SqlServerCe.4.0   就OK了
+ * 
+ * 没有第二部的话  也就是说不还原回去的话 会报如下错误：
+ * [Win32Exception (0x80004005): 找不到网络路径。]
+ * 
+ * [SqlException (0x80131904): 在与 SQL Server 建立连接时出现与网络相关的或特定于实例的错误。未找到或无法访问服务器。请验证实例名称是否正确并且 SQL Server 已配置为允许远程连接。 (provider: Named Pipes Provider, error: 40 - 无法打开到 SQL Server 的连接)]
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{ 
+    public class StoreManagerController : Controller
+    {
+        private MusicStoreEntities db = new MusicStoreEntities();
+
+        //
+        // GET: /StoreManager/
+
+        public ViewResult Index()
+        {
+            var albums = db.Albums.Include(a => a.Genre).Include(a => a.Artist);
+            return View(albums.ToList());
+        }
+
+        //
+        // GET: /StoreManager/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Album album = db.Albums.Find(id);
+            return View(album);
+        }
+
+        //
+        // GET: /StoreManager/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name");
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name");
+            return View();
+        } 
+
+        //
+        // POST: /StoreManager/Create
+
+        [HttpPost]
+        public ActionResult Create(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Albums.Add(album);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+        
+        //
+        // GET: /StoreManager/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Album album = db.Albums.Find(id);
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);//The final parameter is the property to be displayed
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        //
+        // POST: /StoreManager/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(album).State = EntityState.Modified;//we’re finding the current instance of the Album using db.Entry(album) and setting its state to Modified. This tells Entity Framework that we are modifying an existing album as opposed to creating a new one.
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        //
+        // GET: /StoreManager/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Album album = db.Albums.Find(id);
+            return View(album);
+        }
+
+        //
+        // POST: /StoreManager/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Album album = db.Albums.Find(id);
+            db.Albums.Remove(album);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
--- MvcMusicStore/Models/Album.cs
+++ MvcMusicStore/Models/Album.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MvcMusicStore.Models
 {
+    [Bind(Exclude = "AlbumId")]  //Bind – Lists fields to exclude or include when binding parameter or form values to model properties
     public class Album
     {
+        [ScaffoldColumn(false)]  //ScaffoldColumn – Allows hiding fields from editor forms
         public int AlbumId { get; set; }
+
+        [DisplayName("Genre")]  //DisplayName – Defines the text we want used on form fields and validation messages
         public int GenreId { get; set; }
+
+        [DisplayName("Artist")]
         public int ArtistId { get; set; }
+
+        [Required(ErrorMessage = "An Album Title is required")] //Required – Indicates that the property is a required field
+        [StringLength(160)]     //StringLength – Defines a maximum length for a string field
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, 100.00,    //Range – Gives a maximum and minimum value for a numeric field
+            ErrorMessage = "Price must be between 0.01 and 100.00")]
         public decimal Price { get; set; }
+
+        [DisplayName("Album Art URL")]
+        [StringLength(1024)]
         public string AlbumArtUrl { get; set; }
-        public Genre Genre { get; set; }
-        public Artist Artist { get; set; }
+
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }//While we’re there, we’ve also changed the Genre and Artist to virtual properties. This allows Entity Framework to lazy-load them as necessary.
     }
 }
